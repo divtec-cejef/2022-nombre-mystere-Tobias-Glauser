afa567aa08b5c09d209d09aa519060c77c37fab0
Tobias Glauser <94681693+Tobias-Glauser@users.noreply.github.com>
2022-08-30 10:45:36 +0200

    exercice fini

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -7,7 +7,7 @@
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
@@ -18,5 +18,28 @@
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
+
+    const NB_MIN = 1;
+    const NB_MAX = 100;
+
+    const nbMystere = tireNombre(NB_MIN,NB_MAX);
+    let nbEssais = 0;
+    let reponse = 0;
+    let message = `Le nombre à deviner est compris entre ${NB_MIN} et ${NB_MAX}.`;
+
+    do {
+        reponse = parseInt( prompt(message) );
+        nbEssais++;
+        if (reponse === nbMystere) {
+            alert(`Vous avez trouvé après ${nbEssais} essais.`);
+            return;
+        }
+
+        if (reponse > nbMystere) {
+            message = `C'est moins`;
+        } else {
+            message = `C'est plus`;
+        }
+    } while (true);
 
 }()); // main IIFE
